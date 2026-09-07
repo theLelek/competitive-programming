52f251eda5d1f3c2876dafa5217085081686e02f
lelek <heschl.leon@gmail.com>
2026-09-07 16:24:41 +0200

    sync from pc

---
+++ usaco/lasers_and_mirrors.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct coord {
+    int x;
+    int y;
+};
+
+struct element {
+    coord current;
+    coord previous;
+    int distance;
+};
+
+int getDirection(coord &a, coord &b) {
+    if (a.y - b.y > 0) {
+        return 0; // up
+    }
+    if (b.y - a.y > 0) {
+        return 2; // down
+    }
+    if (a.x - b.x > 0) {
+        return 3; // left
+    }
+    if (b.x - a.x > 0) {
+        return 1; // right;
+    }
+    return -1; // equal
+}
+
+void add(deque<element> &q, element &current, coord &next) {
+    int direction1 = getDirection(current.previous, current.current);
+    int direction2 = getDirection(current.previous, current.current) == getDirection(current.current, next);
+    if (direction1 == direction2 || direction1 == -1) {
+        q.push_front({next, current.current, current.distance});
+    } else {
+        q.push_back({next, current.current, current.distance + 1});
+    }
+}
+
+int main() {
+    int n;
+    int x1; int y1;
+    int x2; int y2;
+    cin >> n;
+    cin >> x1; cin >> y1;
+    cin >> x2; cin >> y2;
+
+    vector<vector<coord>> columns(1000000 + 5);
+    vector<vector<coord>> rows(1000000 + 5);
+
+    for (int i = 0; i < n; i++) {
+        int x; int y;
+        cin >> x; cin >> y;
+        coord c = coord(x, y);
+        columns.at(x).push_back(c);
+        rows.at(y).push_back(c);
+    }
+
+    coord target = {x2, y2};
+    columns.at(x2).push_back(target);
+    rows.at(y2).push_back(target);
+
+    coord root = {x1, y1};
+    deque<element> q = {};
+    q.push_back({root, root, 0});
+
+    while (! q.empty()) {
+        element current = q.front();
+        if (current.current == root)
+        q.pop_front();
+        for (int i = 0; i < columns.at(current.current.x).size(); i++) {
+            coord currentCoord = columns.at(current.current.x).at(i);
+            add(q, current, currentCoord);
+        }
+        for (int i = 0; i < rows.at(current.current.y).size(); i++) {
+            coord currentCoord = columns.at(current.current.y).at(i);
+            add(q, current, currentCoord);
+        }
+    }
+    return 0;
+}
