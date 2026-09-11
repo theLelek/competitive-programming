f6b78591c117f53ef554d545c85b39e020788656
lelek <heschl.leon@gmail.com>
2026-09-11 02:11:52 +0200

    attempted lasers and mirrors

--- usaco/lasers_and_mirrors.cpp
+++ usaco/lasers_and_mirrors.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -7,6 +8,10 @@ using namespace std;
 struct coord {
     int x;
     int y;
+
+    bool operator==(const coord &other) {
+        return other.x == x && other.y == y;
+    }
 };
 
 struct element {
@@ -41,6 +46,32 @@ void add(deque<element> &q, element &current, coord &next) {
     }
 }
 
+vector<int> substitute(vector<coord> &coords, bool pickX) {
+    vector<int> out;
+    for (int i = 0; i < coords.size(); i++) {
+        if (pickX) {
+            out.push_back(coords.at(i).x);
+        } else {
+            out.push_back(coords.at(i).y);
+        }
+    }
+    return out;
+}
+
+int compress(vector<int> &list, int target) {
+    int l = 0;
+    int r = list.size();
+    while (l < r) {
+        int m = l + (r - l) / 2;
+        if (list.at(m) >= target) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+    return r;
+}
+
 int main() {
     int n;
     int x1; int y1;
@@ -49,28 +80,45 @@ int main() {
     cin >> x1; cin >> y1;
     cin >> x2; cin >> y2;
 
-    vector<vector<coord>> columns(1000000 + 5);
-    vector<vector<coord>> rows(1000000 + 5);
+
+    vector<coord> coords;
 
     for (int i = 0; i < n; i++) {
         int x; int y;
         cin >> x; cin >> y;
         coord c = coord(x, y);
+        coords.push_back(c);
+    }
+    vector<int> yCoords = substitute(coords, true);
+    vector<int> xCoords = substitute(coords, false);
+    sort(yCoords.begin(), yCoords.end());
+    sort(xCoords.begin(), xCoords.end());
+
+    vector<vector<coord>> columns(100000 + 5);
+    vector<vector<coord>> rows(100000 + 5);
+
+    for (int i = 0; i < n; i++) {
+        int x = compress(xCoords, xCoords.at(i));
+        int y = compress(yCoords, yCoords.at(i));
+        coord c = coord(x, y);
         columns.at(x).push_back(c);
         rows.at(y).push_back(c);
     }
 
-    coord target = {x2, y2};
-    columns.at(x2).push_back(target);
-    rows.at(y2).push_back(target);
+    coord target = {compress(xCoords, x2), compress(yCoords, y2)};
+    columns.at(compress(xCoords, x2)).push_back(target);
+    rows.at(compress(yCoords, y2)).push_back(target);
 
-    coord root = {x1, y1};
+    coord root = {compress(xCoords, x1), compress(yCoords, y1)};
+
     deque<element> q = {};
     q.push_back({root, root, 0});
 
     while (! q.empty()) {
         element current = q.front();
-        if (current.current == root)
+        if (current.current == target) {
+            break;
+        }
         q.pop_front();
         for (int i = 0; i < columns.at(current.current.x).size(); i++) {
             coord currentCoord = columns.at(current.current.x).at(i);
@@ -80,6 +128,12 @@ int main() {
             coord currentCoord = columns.at(current.current.y).at(i);
             add(q, current, currentCoord);
         }
+    }
+
+    if (q.size() == 0) {
+        cout << -1;
+    } else {
+        cout << q.front().distance;
     }
     return 0;
 }
---
+++ usaco/rectangular_pasture.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x; int y;
+        cin >> x; cin >> y;
+        
+
+    }
+
+
+    return 0;
+}
