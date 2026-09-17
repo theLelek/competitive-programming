97ba7eb8ec51412a89ca857d91ed11edd6ce5c85
lelek <heschl.leon@gmail.com>
2026-09-17 21:42:45 +0200

    continued on lasers and mirrors

--- usaco/lasers_and_mirrors.cpp
+++ usaco/lasers_and_mirrors.cpp
@@ -12,12 +12,20 @@ struct coord {
     bool operator==(const coord &other) {
         return other.x == x && other.y == y;
     }
+
+    coord(int x1, int y1) {
+        x = x1;
+        y = y1;
+    }
 };
 
 struct element {
     coord current;
     coord previous;
     int distance;
+
+    element(coord current, coord previous, int distance)
+        : current(current), previous(previous), distance(distance) {}
 };
 
 int getDirection(coord &a, coord &b) {
@@ -38,7 +46,7 @@ int getDirection(coord &a, coord &b) {
 
 void add(deque<element> &q, element &current, coord &next) {
     int direction1 = getDirection(current.previous, current.current);
-    int direction2 = getDirection(current.previous, current.current) == getDirection(current.current, next);
+    int direction2 = getDirection(current.current, next);
     if (direction1 == direction2 || direction1 == -1) {
         q.push_front({next, current.current, current.distance});
     } else {
@@ -80,8 +88,7 @@ int main() {
     cin >> x1; cin >> y1;
     cin >> x2; cin >> y2;
 
-
-    vector<coord> coords;
+    vector<coord> coords = {{x1, y1}, {x2, y2}};
 
     for (int i = 0; i < n; i++) {
         int x; int y;
@@ -89,44 +96,80 @@ int main() {
         coord c = coord(x, y);
         coords.push_back(c);
     }
-    vector<int> yCoords = substitute(coords, true);
-    vector<int> xCoords = substitute(coords, false);
-    sort(yCoords.begin(), yCoords.end());
-    sort(xCoords.begin(), xCoords.end());
+
+    vector<int> yCoordsCompressor = substitute(coords, true);
+    vector<int> xCoordsCompressor = substitute(coords, false);
+    sort(yCoordsCompressor.begin(), yCoordsCompressor.end());
+    sort(xCoordsCompressor.begin(), xCoordsCompressor.end());
+
+    // now coords are compresses
 
     vector<vector<coord>> columns(100000 + 5);
     vector<vector<coord>> rows(100000 + 5);
 
     for (int i = 0; i < n; i++) {
-        int x = compress(xCoords, xCoords.at(i));
-        int y = compress(yCoords, yCoords.at(i));
+        int x = compress(xCoordsCompressor, coords.at(i).x);
+        int y = compress(yCoordsCompressor, coords.at(i).y);
         coord c = coord(x, y);
         columns.at(x).push_back(c);
         rows.at(y).push_back(c);
     }
 
-    coord target = {compress(xCoords, x2), compress(yCoords, y2)};
-    columns.at(compress(xCoords, x2)).push_back(target);
-    rows.at(compress(yCoords, y2)).push_back(target);
+    coord target = {compress(xCoordsCompressor, x2), compress(yCoordsCompressor, y2)};
+    columns.at(compress(xCoordsCompressor, x2)).push_back(target);
+    rows.at(compress(yCoordsCompressor, y2)).push_back(target);
 
-    coord root = {compress(xCoords, x1), compress(yCoords, y1)};
+    coord root = {compress(xCoordsCompressor, x1), compress(yCoordsCompressor, y1)};
 
     deque<element> q = {};
     q.push_back({root, root, 0});
-
-    while (! q.empty()) {
+    vector<bool> visitedRows(n + 5);
+    vector<bool> visitedColumns(n + 5);
+    //
+    // while (! q.empty()) {
+    //     element current = q.front();
+    //     if (current.current == target) {
+    //         break;
+    //     }
+    //     q.pop_front();
+    //     for (int i = 0; i < columns.at(current.current.x).size(); i++) {
+    //         if (visitedRows.at(current.current.y)) break;
+    //         coord currentCoord = columns.at(current.current.x).at(i);
+    //         add(q, current, currentCoord);
+    //     }
+    //
+    //     for (int i = 0; i < rows.at(current.current.y).size(); i++) {
+    //         if (visitedColumns.at(current.current.x)) break;
+    //         coord currentCoord = rows.at(current.current.y).at(i);
+    //         add(q, current, currentCoord);
+    //     }
+    //
+    //     visitedRows.at(current.current.y) = true;
+    //     visitedColumns.at(current.current.x) = true;
+    // }
+    while (!q.empty()) {
         element current = q.front();
+
         if (current.current == target) {
             break;
         }
+
         q.pop_front();
-        for (int i = 0; i < columns.at(current.current.x).size(); i++) {
-            coord currentCoord = columns.at(current.current.x).at(i);
-            add(q, current, currentCoord);
+
+        if (!visitedColumns[current.current.x]) {
+            for (int i = 0; i < columns[current.current.x].size(); i++) {
+                coord currentCoord = columns[current.current.x][i];
+                add(q, current, currentCoord);
+            }
+            visitedColumns[current.current.x] = true;
         }
-        for (int i = 0; i < rows.at(current.current.y).size(); i++) {
-            coord currentCoord = columns.at(current.current.y).at(i);
-            add(q, current, currentCoord);
+
+        if (!visitedRows[current.current.y]) {
+            for (int i = 0; i < rows[current.current.y].size(); i++) {
+                coord currentCoord = rows[current.current.y][i];
+                add(q, current, currentCoord);
+            }
+            visitedRows[current.current.y] = true;
         }
     }
 
