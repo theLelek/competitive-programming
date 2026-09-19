6c2dcaca13bb975943e2d7c708b7faa2d48d1fb0
lelek <heschl.leon@gmail.com>
2026-09-19 11:59:09 +0200

    wip construct a palindrome

--- atcoder/f_construct_a_palindrome.cpp
+++ atcoder/f_construct_a_palindrome.cpp
@@ -7,10 +7,53 @@ using namespace std;
 vector<char> letters;
 vector<vector<int>> adjList;
 
-struct element {
-    vector<int> nodes;
-    vector<int> distance;
+struct node {
+    int index;
+    int distance;
 };
+
+struct element {
+    vector<node> nodes;
+};
+
+queue<node> qLeft;
+queue<node> qRight;
+vector<element> doesCharExistLeft(26); // chars - 97
+
+void bfsLeft() {
+    while (! qLeft.empty()) {
+        node current = qLeft.front();
+        qLeft.pop();
+        for (int child : adjList.at(current.index)) {
+            char letter = letters.at(child);
+            doesCharExistLeft.at((int) letter - 97).nodes.push_back({child, current.distance + 1});
+        }
+    }
+}
+
+void bfsRight() {
+    vector<bool> alreadyAdded(26);
+    queue<node> qRight2;
+    while (! qRight.empty()) {
+        node current = qRight.front();
+        qRight.pop();
+        for (int child : adjList.at(current.index)) {
+            char letter = letters.at(child);
+            if (doesCharExistLeft.at((int) letter - 97).nodes.empty()) {
+                continue;
+            }
+            qRight2.push({child, current.distance + 1});
+            if (alreadyAdded.at((int) letter - 97)) {
+                continue;
+            }
+            for (node n : doesCharExistLeft.at((int) letter - 97).nodes) {
+                qLeft.push(n);
+            }
+            alreadyAdded.at((int) letter - 97) = true;
+        }
+    }
+    qRight = qRight2;
+}
 
 int main() {
     int n; int m;
@@ -27,10 +70,10 @@ int main() {
         adjList.at(b).push_back(a);
     }
 
-    vector<element> doesCharExist(26); // chars - 97
-
-    queue<element> q = {};
-    q.push(element({1}, {0}));
+    qLeft.push({1, 0});
+    qRight.push({n, 0});
+    bfsLeft();
+    bfsRight();
 
     return 0;
 }
