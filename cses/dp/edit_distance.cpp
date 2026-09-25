b51a2010ae1a8f962f3d95dc076bdb90ff790dec
lelek <heschl.leon@gmail.com>
2026-09-25 17:00:31 +0200

    finished top down approach

--- cses/dp/edit_distance.cpp
+++ cses/dp/edit_distance.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <limits.h>
+#include <vector>
 
 using namespace std;
 
 string s1; string s2;
 
+vector<vector<int>> dp;
+
 int solveRecursively(int idx1, int idx2) {
-    if (idx1 == s1.size() || idx2 == s2.size()) {
-        return INT_MAX / 2;
-    }
-    if (idx1 == s1.size() + 1 || idx2 == s2.size() + 1) {
-        return 0;
-    }
+    if (idx1 == s1.size())
+        return s2.size() - idx2;
+
+    if (idx2 == s2.size())
+        return s1.size() - idx1;
 
     // adds before idx1
     int ans1 = 1 + solveRecursively(idx1, idx2 + 1);
@@ -27,12 +29,15 @@ int solveRecursively(int idx1, int idx2) {
     // remove
     int ans3 = 1 + solveRecursively(idx1 + 1, idx2);
 
-    int ans = min(ans1, max(ans2, ans3));
+    int ans = min(ans1, min(ans2, ans3));
     return ans;
 }
 
 int main() {
     cin >> s1; cin >> s2;
+
+    dp.resize(s1.size() + 5, vector<int>(s2.size() + 5));
+
     cout << solveRecursively(0, 0);
     return 0;
 }
